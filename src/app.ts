673827c6b241a2e2bcc055670f2a34e71c68e845
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2021-10-15 19:37:17 +0700

    Setup eslint

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('Hello, world!')
+import http2 from 'http2'
+import fs from 'fs'
+
+const server = http2.createSecureServer({
+  key: fs.readFileSync('ssl/localhost-privkey.pem'),
+  cert: fs.readFileSync('ssl/localhost-cert.pem')
+})
+server.on('error', (err) => console.error(err))
+  
+server.on('stream', (stream, headers) => {
+  // stream is a Duplex
+  stream.respond({
+    'content-type': 'text/html; charset=utf-8',
+    ':status': 200
+  })
+  stream.end('<h1>Hello World</h1>')
+})
+  
+server.listen(8443)
