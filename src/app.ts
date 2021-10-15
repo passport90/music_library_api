cbfdfe8ca8d54c5be4c762685654b93da35d0d92
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2021-10-15 19:48:15 +0700

    Fix eslint no unused vars

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,13 @@ const server = http2.createSecureServer({
 })
 server.on('error', (err) => console.error(err))
   
-server.on('stream', (stream, headers) => {
+server.on('stream', (stream, _headers) => {
   // stream is a Duplex
   stream.respond({
     'content-type': 'text/html; charset=utf-8',
     ':status': 200
   })
-  stream.end('<h1>Hello World</h1>')
+  stream.end('Hello, World!')
 })
   
 server.listen(8443)
