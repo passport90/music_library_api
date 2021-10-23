c260817e00509da353f076c5dd63f13b88c2a7a6
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2021-10-23 12:00:21 +0700

    sort-safe artistIndex

--- src/actions/artistIndexAction.ts
+++ src/actions/artistIndexAction.ts
@@ -11,8 +11,17 @@ const artistIndexAction: Action = async (
 ): Promise<Response> => {
   const res = await pgClient.query('select id, name from artist order by name')
   const artists = res.rows
+
+  const displayArtists = artists.map((artist) => {
+    const matches = artist.name.match(/^(.+), The$/)
+    let displayArtistName
+    if (matches !== null) {
+      displayArtistName = `The ${matches[1]}`
+    }
+    return { ...artist, name: displayArtistName ?? artist.name }
+  })
   
-  return { status: 200, body: { artists } }
+  return { status: 200, body: { artists: displayArtists } }
 }
 
 export default artistIndexAction
