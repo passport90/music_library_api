cc6e7f930acd28cf089f70ed8d6eb29b971384a1
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2021-10-25 17:34:45 +0700

    Update hello.ts

--- src/hello.ts
+++ src/hello.ts
@@ -2,7 +2,7 @@ import http from 'http'
 
 // Run HTTP OK server
 const httpServer = http.createServer((_req, res) => {
-  res.writeHead(204)
-  res.end()
+  res.writeHead(200)
+  res.end('hello, world!')
 })
 httpServer.listen(8080)
