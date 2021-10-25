17ba25f00443de25d39b4106e51ef681b10dd840
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2021-10-25 20:40:01 +0700

    set hello server to serve contents of file

--- src/hello.ts
+++ src/hello.ts
@@ -1,8 +1,18 @@
 import http from 'http'
+import fs from 'fs'
 
-// Run HTTP OK server
-const httpServer = http.createServer((_req, res) => {
-  res.writeHead(200)
-  res.end('hello, world!')
+// Run HTTP OK server that open file
+const httpServer = http.createServer((req: http.IncomingMessage, res) => {
+  const path = req.url
+  
+  try {
+    const file = fs.readFileSync(`${process.env.PUBLIC_PATH}${path}`)
+    res.writeHead(200)
+    res.end(file.toString())
+  } catch (error) {
+    const message = error instanceof Error ? error.message : 'Unknown error'
+    res.writeHead(500)
+    res.end(message)
+  }
 })
 httpServer.listen(8080)
